33790b18cd144b3a368aba2366cb1477aed4cdf0
Dominic Hopton <d.hopton@codevoid.net>
2019-12-30 15:22:14 -0800

    Placeholder Twitter API Keys
    This file is intentionally incomplete, and will be marked as 'don't
    track changes' for future usages using `git update-index
    --skip-worktree`

---
+++ OAuthLibraryTests/TwitterAPIKey.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Codevoid.Tests.OAuth
+{
+    /// <summary>
+    /// Holds the API Key for twitter so that you can run tests that actually
+    /// do real work. Note, you need to get your own key, and place it in the
+    /// appropriate constants below
+    /// </summary>
+    public static class TwitterAPIKey
+    {
+        public const string CLIENT_ID = "PLACEHOLDER";
+        public const string CLIENT_SECRET = "PLACEHOLDER";
+        public const string ACCESS_TOKEN = "PLACEHOLDER";
+        public const string TOKEN_SECRET = "PLACEHOLDER";
+    }
+}
