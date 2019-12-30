09edee8c71fa4e5b0a8279747036b1ab57716228
Dominic Hopton <d.hopton@codevoid.net>
2019-12-30 14:59:40 -0800

    Added Parameter Encoder implementation
    - Made it a static helper & added arguments for configuring delimiter,
    quoting
    - Added Unit Tests

--- OAuthLibrary/ParameterEncoder.cs
+++ OAuthLibrary/ParameterEncoder.cs
@@ -1,8 +1,35 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace Codevoid.Utilities.OAuth
 {
-    internal class ParameterEncoder
+    internal static class ParameterEncoder
     {
+        internal static string FormEncodeValues(IDictionary<string, string> valuesToEncode, string delimiter = "&", bool shouldQuoteValues = false)
+        {
+            IDictionary<string, string> encodedValues =
+                new SortedDictionary<string, string>(StringComparer.Ordinal);
+
+            foreach (var keyAndValueToEncode in valuesToEncode)
+            {
+                var encodedKey = Uri.EscapeDataString(keyAndValueToEncode.Key);
+                var encodedValue = Uri.EscapeDataString(keyAndValueToEncode.Value);
+                encodedValues.Add(encodedKey, encodedValue);
+            }
+
+            var sb = new StringBuilder();
+            var quoteChar = (shouldQuoteValues ? "\"" : "");
+
+            foreach (var encodedKeyAndValue in encodedValues)
+            {
+                sb.Append($"{encodedKeyAndValue.Key}={quoteChar}{encodedKeyAndValue.Value}{quoteChar}{delimiter}");
+            }
+
+            // Remove the trailing & we added
+            sb.Length -= delimiter.Length;
+
+            return sb.ToString();
+        }
     }
 }
--- OAuthLibraryTests/ParameterEncoderTests.cs
+++ OAuthLibraryTests/ParameterEncoderTests.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Codevoid.Utilities.OAuth;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -7,10 +8,66 @@ namespace Codevoid.Test.Utilities.OAuth
     public class ParameterEncoderTests
     {
         [TestMethod]
-        public void CanConstructParameterEncoder()
+        public void OneParameterAndValueEncodes()
         {
-            var instance = new ParameterEncoder();
-            Assert.IsNotNull(instance, "Encoder instance was null");
+            var sampleData = new Dictionary<string, string>
+            {
+                { "a", "b" }
+            };
+
+            var result = ParameterEncoder.FormEncodeValues(sampleData);
+            Assert.AreEqual("a=b", result, "Encoding string didn't match");
+        }
+
+        [TestMethod]
+        public void TwoParametersAndValueEncodesWithCorrectOrder()
+        {
+            var sampleData = new Dictionary<string, string>
+            {
+                { "b", "c%jkt" },
+                { "a", "b" }
+            };
+
+            var result = ParameterEncoder.FormEncodeValues(sampleData);
+            Assert.AreEqual("a=b&b=c%25jkt", result, "Encoding string didn't match");
+        }
+
+        [TestMethod]
+        public void ValuesAreEncodedAccordingToRFC3986()
+        {
+            var sampleData = new Dictionary<string, string>
+            {
+                { "!'()*", "*)('!" }
+            };
+
+            var result = ParameterEncoder.FormEncodeValues(sampleData);
+            Assert.AreEqual("%21%27%28%29%2A=%2A%29%28%27%21", result, "Encoding string didn't match");
+        }
+
+        [TestMethod]
+        public void CustomDelimiterIsRespectedIfProvided()
+        {
+            var sampleData = new Dictionary<string, string>
+            {
+                { "b", "c%jkt" },
+                { "a", "b" }
+            };
+
+            var result = ParameterEncoder.FormEncodeValues(sampleData, delimiter: ", ");
+            Assert.AreEqual("a=b, b=c%25jkt", result, "Encoding string didn't match");
+        }
+
+        [TestMethod]
+        public void ValuesAreQuotedWhenRequested()
+        {
+            var sampleData = new Dictionary<string, string>
+            {
+                { "b", "c%jkt" },
+                { "a", "b" }
+            };
+
+            var result = ParameterEncoder.FormEncodeValues(sampleData, shouldQuoteValues: true);
+            Assert.AreEqual("a=\"b\"&b=\"c%25jkt\"", result, "Encoding string didn't match");
         }
     }
 }
