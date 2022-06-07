626efdd7eb50dff7d1aabff589adbfc4b9fc6e75
Dominic Hopton <d.hopton@codevoid.net>
2022-06-07 14:25:19 -0700

    Update Key Templates to use file namespace

--- TwitterAPIKey.cs
+++ TwitterAPIKey.cs
@@ -1,19 +1,18 @@
-﻿namespace Codevoid.Test.OAuth
+﻿namespace Codevoid.Test.OAuth;
+
+/// <summary>
+/// Holds the API Key for twitter so that you can run tests that actually do
+/// real work. Note, you need to get your own key, and place it in the
+/// appropriate constants below.
+///
+/// Twitter Tokens & IDs. Replace with tokens & IDs that are issued to you
+/// from Twitters developer portal:
+/// https://developer.twitter.com/en/docs/developer-portal/overview
+/// </summary>
+public static class TwitterAPIKey
 {
-    /// <summary>
-    /// Holds the API Key for twitter so that you can run tests that actually do
-    /// real work. Note, you need to get your own key, and place it in the
-    /// appropriate constants below.
-    ///
-    /// Twitter Tokens & IDs. Replace with tokens & IDs that are issued to you
-    /// from Twitters developer portal:
-    /// https://developer.twitter.com/en/docs/developer-portal/overview
-    /// </summary>
-    public static class TwitterAPIKey
-    {
-        public const string API_KEY = "PLACEHOLDER";
-        public const string API_SECRET_KEY = "PLACEHOLDER";
-        public const string ACCESS_TOKEN = "PLACEHOLDER";
-        public const string ACCESS_TOKEN_SECRET = "PLACEHOLDER";
-    }
+    public const string API_KEY = "PLACEHOLDER";
+    public const string API_SECRET_KEY = "PLACEHOLDER";
+    public const string ACCESS_TOKEN = "PLACEHOLDER";
+    public const string ACCESS_TOKEN_SECRET = "PLACEHOLDER";
 }
