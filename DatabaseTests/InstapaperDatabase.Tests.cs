abafb6d6e0b1e9e8bd1c71176138a58da5164b7b
Dominic Hopton <d.hopton@codevoid.net>
2022-06-29 16:21:03 -0700

    Update DB Reopen test to persist something
    Rather than hope we can 'just repopen it' 'casue, well, we opened it,
    lets put _something_ in it, and get that _something_ back

--- DatabaseTests/InstapaperDatabase.Tests.cs
+++ DatabaseTests/InstapaperDatabase.Tests.cs
@@ -8,11 +8,45 @@ public sealed class InstapaperDatabaseTests
     [Fact]
     public void CanReopenDatabase()
     {
-        using var connection = new SqliteConnection("Data Source=:memory:");
-        InstapaperDatabase.OpenOrCreateDatabase(connection);
+        const string CONNECTION_STRING = "Data Source=StaysInMemory;Mode=Memory;Cache=Shared";
+        // To keep data in an in-memory DB, we have to maintain at least _one_
+        // shared cache connection to that db.
+        using var baseConnection = new SqliteConnection(CONNECTION_STRING);
+        baseConnection.Open();
 
-        connection.Close();
+        // Open the DB, and put _something_ in it
+        using (var connection1 = new SqliteConnection(CONNECTION_STRING))
+        {
+            InstapaperDatabase.OpenOrCreateDatabase(connection1);
 
-        InstapaperDatabase.OpenOrCreateDatabase(connection);
+            // Why not use the data access layer? To keep the tests decoupled.
+            // This does run the risk of atrophy over time, but removing the
+            // coupling seems worthy of that risk
+            using var query = connection1.CreateCommand(@"
+                INSERT INTO folders(title, local_id)
+                VALUES ('Sample', -1);
+            ");
+
+            query.ExecuteScalar();
+
+            connection1.Close();
+        }
+
+        // Reopen the db and get _something_ out of it
+        using (var connection2 = new SqliteConnection(CONNECTION_STRING))
+        {
+            InstapaperDatabase.OpenOrCreateDatabase(connection2);
+            using var query = connection2.CreateCommand(@"
+                SELECT COUNT(local_id) FROM folders
+                WHERE local_id = -1
+            ");
+
+            var count = (long)(query.ExecuteScalar()!);
+            Assert.Equal(1, count);
+
+            connection2.Close();
+        }
+
+        baseConnection.Close();
     }
 }
