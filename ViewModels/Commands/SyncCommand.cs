bc973b482c7ef2c630c21869f3aaf1d15b6bc32c
Dominic Hopton <d.hopton@codevoid.net>
2023-01-06 11:55:13 -0800

    Add Sync command
    This command lets us initiate — and represent the status of that sync — from a single command across the app. Yay.

---
+++ ViewModels/Commands/SyncCommand.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Windows.Input;
+using Codevoid.Storyvoid.Sync;
+
+namespace Codevoid.Storyvoid.ViewModels.Commands;
+
+/// <summary>
+/// Initiates app-wide syncing of the database &amp; article contents
+/// </summary>
+public class SyncCommand : ICommand, IDisposable
+{
+    public event EventHandler? CanExecuteChanged;
+    private SyncHelper helper;
+
+    /// <summary>
+    /// Instantiates a command wrapping a given instance of the sync helper. It
+    /// is expected -- but not required -- that this be a shared instance for a
+    /// given database.
+    /// </summary>
+    /// <param name="helper"></param>
+    public SyncCommand(SyncHelper helper)
+    {
+        this.helper = helper;
+        this.helper.PropertyChanged += Helper_PropertyChanged;
+    }
+
+    private void Helper_PropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
+    {
+        this.RaiseCanExecuteChanged();
+    }
+
+    public void Dispose()
+    {
+        this.helper.PropertyChanged -= Helper_PropertyChanged;
+    }
+
+    private void RaiseCanExecuteChanged()
+    {
+        this.CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+    }
+
+    // When syncing, we should be disabled so as not to initiate another sync
+    public bool CanExecute(object? parameter) => !this.helper.IsSyncing;
+
+    public async void Execute(object? parameter)
+    {
+        await this.helper.SyncDatabaseAndArticles();
+    }
+}
---
+++ ViewModelsTests/SyncCommand.Tests.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Data;
+using Codevoid.Storyvoid;
+using Codevoid.Storyvoid.Sync;
+using Codevoid.Storyvoid.ViewModels.Commands;
+using Microsoft.Data.Sqlite;
+
+namespace Codevoid.Test.Storyvoid.ViewModels;
+
+public class SyncCommandTests : IDisposable
+{
+    private class MockInstapaperSync : IInstapaperSync
+    {
+        private TaskCompletionSource tcs = new TaskCompletionSource();
+        public async Task SyncEverythingAsync(CancellationToken cancellationToken = default)
+        {
+            await this.tcs.Task;
+            cancellationToken.ThrowIfCancellationRequested();
+        }
+
+        public void TriggerSyncEverythingComplete()
+        {
+            this.tcs.SetResult();
+        }
+    }
+
+    private class MockArticleDownloader : IArticleDownloader
+    {
+        private TaskCompletionSource downloadAllWithoutState = new TaskCompletionSource();
+        private TaskCompletionSource downloadAllArticles = new TaskCompletionSource();
+        private TaskCompletionSource<DatabaseLocalOnlyArticleState?> downloadSingleArticleState = new TaskCompletionSource<DatabaseLocalOnlyArticleState?>();
+
+        public Task DownloadAllArticlesWithoutLocalStateAsync(CancellationToken cancellationToken = default) => this.downloadAllWithoutState.Task;
+        public Task<DatabaseLocalOnlyArticleState?> DownloadArticleAsync(DatabaseArticle article, CancellationToken cancellationToken = default) => this.downloadSingleArticleState.Task;
+        public Task DownloadArticlesAsync(IEnumerable<DatabaseArticle> articles, CancellationToken cancellationToken = default) => this.downloadAllArticles.Task;
+
+        public void TriggerAll()
+        {
+            this.downloadAllArticles.SetResult();
+            this.downloadAllWithoutState.SetResult();
+            this.downloadSingleArticleState.SetResult(null);
+        }
+    }
+
+    private SyncCommand command;
+    private SyncHelper helper;
+    private MockInstapaperSync mockSync;
+    private MockArticleDownloader mockDownloader;
+
+    public SyncCommandTests()
+    {
+        this.mockSync = new MockInstapaperSync();
+        this.mockDownloader = new MockArticleDownloader();
+        this.helper = new SyncHelper(mockDownloader, () =>
+        {
+            var connection = new SqliteConnection("Data Source=:memory:");
+            connection.Open();
+
+
+            return Task.FromResult<(IInstapaperSync, IDbConnection)>((this.mockSync, connection));
+        });
+
+        this.command = new SyncCommand(this.helper);
+    }
+
+    public void Dispose()
+    {
+        this.command.Dispose();
+    }
+
+    private void TriggerSyncingComplete()
+    {
+        this.mockDownloader.TriggerAll();
+        this.mockSync.TriggerSyncEverythingComplete();
+    }
+
+    [Fact]
+    public void CanInstantiateCommand()
+    {
+        Assert.NotNull(this.command);
+    }
+
+    [Fact]
+    public void CanExecuteReturnsTrueWhenNotSyncing()
+    {
+        Assert.True(this.command.CanExecute(null));
+    }
+
+    [Fact]
+    public async Task CanExecuteReturnsFalseWhenSyncing()
+    {
+        var syncTask = this.helper.SyncDatabaseAndArticles();
+
+        Assert.False(this.command.CanExecute(null));
+
+        this.TriggerSyncingComplete();
+
+        await syncTask;
+    }
+
+    [Fact]
+    public async Task CanExecuteReturnsTrueAfterSyncingComplete()
+    {
+        this.TriggerSyncingComplete();
+        await this.helper.SyncDatabaseAndArticles();
+
+        Assert.True(this.command.CanExecute(null));
+    }
+
+    [Fact]
+    public async Task CanExexcuteChangedRaisedCorrectlyDuringSync()
+    {
+        var completionSource = new TaskCompletionSource();
+        var canExecuteResults = new List<bool>();
+        this.command.CanExecuteChanged += (o, a) =>
+        {
+            canExecuteResults.Add(this.command.CanExecute(null));
+            if (canExecuteResults.Count == 2)
+            {
+                completionSource.SetResult();
+            }
+        };
+
+        this.TriggerSyncingComplete();
+
+        this.command.Execute(null);
+
+        await completionSource.Task;
+
+        Assert.Equal(2, canExecuteResults.Count);
+        Assert.False(canExecuteResults[0]);
+        Assert.True(canExecuteResults[1]);
+    }
+}
+
