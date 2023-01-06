7724a83c8b781fe1d1f3d11166f6d255c1be28ba
Dominic Hopton <d.hopton@codevoid.net>
2023-01-06 15:51:20 -0800

    Add `DownloadArticleCommand` to download articles
    No unit tests ‘cause it’s such a simple command.

---
+++ ViewModels/Commands/DownloadArticleCommand.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Codevoid.Storyvoid.Sync;
+
+namespace Codevoid.Storyvoid.ViewModels.Commands;
+
+internal class DownloadArticleCommand : ArticleCommand
+{
+    private IArticleDownloader downloader;
+    public DownloadArticleCommand(IArticleDownloader downloader, IArticleDatabase database) : base(database)
+    {
+        this.downloader = downloader;
+    }
+
+    protected override async void CoreExecute(DatabaseArticle article)
+    {
+        try
+        {
+            await this.downloader.DownloadArticleAsync(article);
+        }
+        catch (Exception) { }
+    }
+}
